e19d7fc0b1805ff3523754fee88ad8c70934d91f
AcostaF2001 <98819485+AcostaF2001@users.noreply.github.com>
2022-09-13 19:54:26 -0500

    CAMBIOS HAY EN MI
    asfasfd

---
+++ src/components/Father.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useState } from 'react'
+import { Son } from './Son'
+
+export const Father = () => {
+
+    const list =[2,4,6,8,10]
+    const [valor,setValor] = useState(0)
+
+    const increment = (num) =>{
+        setValor(valor+num)
+    }
+  return (
+    <div>
+        <h1>Father</h1>
+        <p>total: {valor}</p>
+        <br />
+        {
+             list.map((n,idx) => {
+                return(
+                    <Son
+                        key={idx}
+                        numero={n}
+                        increment={increment}
+                    />
+                
+                )
+             }) 
+
+            
+
+
+        }
+           
+        
+        </div>
+
+        
+        
+  )
+}
+
--- src/components/getGifs.js
+++ src/components/getGifs.js
@@ -1,17 +1,15 @@
-export const getGifs = async(category) =>{
-    const url=`https://api.giphy.com/v1/gifs/search?api_key=XEd4gVo1xrS2YsPs09wjb8q3AdiJ3Lwc&q=${category}&limit=25&offset=0&rating=g&lang=en
+export const  getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=SxBTpzUqdOLJ9TfJXWZacUkBzqhmEQM9&q=${category}&limit=25&offset=0&rating=g&lang=en`
 
-    `
-    const resp= await fetch(url)
-    const {data} = await resp.json()
-    const gifs = data.map(img =>{
-        return{
-            id:img.id,
-            title:img.title,
-            url:img.images.downsized_medium.usl
 
+    const resp = await fetch(url) //consume la api
+    const {data} = await resp.json() //llama a la data
+    const gifs = data.map ( img => {
+        return { //
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
         }
-    })
-
+    }) 
     return gifs
 }
